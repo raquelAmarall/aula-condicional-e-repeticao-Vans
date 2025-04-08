1f6e1deebc5320c64c008d039ca20ea45123cabd
Raquel <raquel.oliamaral@gmail.com>
2025-04-08 10:13:11 -0300

    login parte 1

---
+++ login-simples.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int senha;
+    do {
+        printf("Digite a senha: ");
+        scanf("%d", &senha);
+        if (senha != 1234){
+            printf("Senha incorreta\n");
+        }
+    }while (senha != 4321);
+
+
+
+   return 0;
+
+}
