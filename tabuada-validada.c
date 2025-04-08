ec39229428aeb3d7b88c5e227a8401eefdaf9b05
Giovana <giovana.dvalentini@gmail.com>
2025-04-08 09:50:33 -0300

    Criando a entrada e validação do tabuada-validada.c

---
+++ tabuada-validada.c
@@ -0,0 +1,28 @@
+/*Tabuada
+crie um programa que: solicite ao usuario um num entre 1 a 10
+caso o valor seja invlidao, use continue para solicitar novamente
+quando o valor for valido, imprima a tabuada completa (1 a 10 + laco for com impressao)
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    while (1)
+    {
+        printf("Digite um numero entre 1 a 10: ");
+        scanf("%d", &num);
+
+        if (num >= 1 && num <= 10)
+        {
+            break;
+        }
+        else
+        {
+            printf("Digite novamente um numero entre 1 a 10: ");
+            continue;
+        }
+    }
+}
