8c911c7fd0123f49a2c123aa0f2e3e338fc23167
Giovana <giovana.dvalentini@gmail.com>
2025-04-15 08:03:05 -0300

    Adicionando os cálculos para cada case

--- menuinterativo.c
+++ menuinterativo.c
@@ -1,31 +1,43 @@
 #include <stdio.h>
 
-int main() {
-    int opcao; 
+int main()
+{
+    int opcao;
 
-    printf("Escolha uma opção (1 a 3): ");  
+    while (1)
+    {
 
-    scanf("%d", &opcao); 
+        printf("Escolha uma opção (1 a 3): ");
+        scanf("%d", &opcao);
 
-    switch (opcao) {
+        switch (opcao)
+        {
+            int numero;
         case 1:
-            printf("Você escolheu a opção 1.\n");
+            printf("Digite um numero inteiro: .\n");
+            scanf("%d", &numero);
+            printf("O numero %d e %s. \n", numero, (numero % 2 == 0) ? "par" : "ímpar");
             break;
 
         case 2:
-            printf("Você escolheu a opção 2.\n");
+            printf("Números de 1 a 10:\n");
+            for (int i = 1; i <= 10; i++)
+            {
+                printf("%d ", i);
+            }
+            printf("\n");
+
             break;
 
         case 3:
-            printf("Você escolheu a opção 3.\n");
-            break;
+            printf("Saindo do programa...\n");
+            return 0;
 
         default:
             printf("Opção inválida!\n");
             break;
+        }
     }
-
-    
 
     return 0;
 }
